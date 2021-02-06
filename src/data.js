3c505bd58c86c0dfc4dea39d61fe5688684bcf9d
Karly-G <gonzalezcervantes.karla@gmail.com>
2021-02-05 21:53:51 -0600

    HU4 terminada con detalle en las funciones

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // Aquí van las Funciones : estas funciones son de ejemplo
 
-import lol from './data/lol/lol.js';
+//import lol from './data/lol/lol.js';
 export const example = () => {
   return 'example';
 };
@@ -8,3 +8,82 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//Filtrado para categoría de Asesinos
+export function filterData1(champions,){
+  let resultAssassin = champions.filter(champion => {
+    let assassin = (champion.tags[0] === "Assassin") + (champion.tags[1] === "Assassin");
+    return assassin;
+  })
+  return resultAssassin;
+}
+
+//Filtrado para categoría de Luchadores
+export function filterData2(champions,){
+  let resultFighter = champions.filter(champion => {
+    let fighter = (champion.tags[0] === "Fighter") + (champion.tags[1] === "Fighter");
+    return fighter;
+  })
+  return resultFighter;
+}
+
+//Filtrado para categoría de Magos
+export function filterData3(champions,){
+  let resultMage = champions.filter(champion => {
+    let mage = (champion.tags[0] === "Mage") + (champion.tags[1] === "Mage");
+    return mage;
+  })
+  return resultMage;
+}
+
+//Filtrado para categoría de Tiradores
+export function filterData4(champions,){
+  let resultMarksman = champions.filter(champion => {
+    let marksman = (champion.tags[0] === "Marksman") + (champion.tags[1] === "Marksman");
+    return marksman;
+  })
+  return resultMarksman;
+}
+
+//Filtrado para categoría de Soportes
+export function filterData5(champions,){
+  let resultSupport = champions.filter(champion => {
+    let support = (champion.tags[0] === "Support") + (champion.tags[1] === "Support");
+    return support;
+  })
+  return resultSupport;
+}
+
+//Filtrado para categoría de Tanques
+export function filterData6(champions,){
+  let resultTank = champions.filter(champion => {
+    let tank = (champion.tags[0] === "Tank") + (champion.tags[1] === "Tank");
+    return tank;
+  })
+  return resultTank;
+}
+
+//-----------FUNCIÓN ORDENAR-----------//
+// export function sortData(champions   /*, sortBy, sortOrder*/){
+//   let orderZA = champions.sort((b, a) =>{
+//   if(b.name < a.name){
+//       return 1;
+//   }
+//   return -1;
+//   });
+//   return orderZA;
+//   }
+//   //------------------------------------//
+  
+
+// export const filter = (champions, id) => {
+//   //     const tags = champions.forEach(champion => champion.tag.filter(tag => tag === id));
+//   //     createCards(tags)
+//   // }
+  
+// let result = champions.filter(champion => champion.tags.Assassin === tags.Assassin);
+// //   let assassin = (champion.tags[0] === "Assassin") + (champion.tags[1] === "Assassin");
+// //   return assassin;
+// // })
+// return result;
+// }
--- src/main.js
+++ src/main.js
@@ -1,41 +1,251 @@
 //Eventos del DOM   
-//import { example } from './data.js';
+import { filterData1, filterData2, filterData3, filterData4, filterData5, filterData6 } from './data.js';
+//import { sortData } from './data.js';
+// import { filterData3 } from './data.js';
+// import { filterData4 } from './data.js';
+// import { filterData5 } from './data.js';
+// import { filterData6 } from './data.js';
 import data from './data/lol/lol.js';
 //console.log(example, data);
-//Aquí estamos conviertiendo nuestro objeto de objetos en un array
+//Aquí estamos conviertiendo nuestro objeto de objetos en un array//
 const champions = Object.values(data.data);
+const championsNum = champions.length;
+document.getElementById('numChampions').innerHTML = championsNum;
 const newCard = document.getElementById("cards-container");
-champions.forEach(champion => {
+///////////////////////////////////////////////////////////////////////
+let filterAssassin = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR ASESINOS
+document.querySelector('#assassin').addEventListener('change', (event) =>{
+    if(event.target.checked){ 
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterAssassin1 = filterData1(champions, 'Assassin');
+        filterAssassin1.forEach(elemento => {
+            let resultAssassin = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;  
+            filterAssassin.insertAdjacentHTML("beforeend", resultAssassin);
+            });
+            let numAssassin = filterAssassin1.length;
+            document.getElementById('numChampions').innerHTML = numAssassin; 
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        }
+    })
+
+
+let filterFighter = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR LUCHADORES
+document.querySelector('#fighter').addEventListener('change', (event) =>{
+    if(event.target.checked){ 
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterFighter1 = filterData2(champions, 'Fighter');
+        filterFighter1.forEach(elemento => {
+            let resultFighter = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;
+            filterFighter.insertAdjacentHTML("beforeend", resultFighter);
+            });
+            let numFighter = filterFighter1.length;
+            document.getElementById('numChampions').innerHTML = numFighter;
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        } 
+    })
+
+
+let filterMage = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR MAGOS
+document.querySelector('#mage').addEventListener('change', (event) =>{
+    if(event.target.checked){ 
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterMage1 = filterData3(champions, 'Mage');
+        filterMage1.forEach(elemento => {
+            let resultMage = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;
+            filterMage.insertAdjacentHTML("beforeend", resultMage);
+            });
+            let numMage = filterMage1.length;
+            document.getElementById('numChampions').innerHTML = numMage; 
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        } 
+    })
+
+
+let filterMarksman = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR TIRADORES
+document.querySelector('#marksman').addEventListener('change', (event) =>{
+    if(event.target.checked){ 
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterMarksman1 = filterData4(champions, 'Marksman');
+        filterMarksman1.forEach(elemento => {
+            let resultMarksman = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;
+            filterMarksman.insertAdjacentHTML("beforeend", resultMarksman);
+            });
+            let numMarksman = filterMarksman1.length;
+            document.getElementById('numChampions').innerHTML = numMarksman; 
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        } 
+    })
+
+
+let filterSupport = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR SOPORTES
+document.querySelector('#support').addEventListener('change', (event) =>{
+    if(event.target.checked){ 
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterSupport1 = filterData5(champions, 'Support');
+        filterSupport1.forEach(elemento => {
+            let resultSupport = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;
+            filterSupport.insertAdjacentHTML("beforeend", resultSupport);
+            });
+            let numSupport = filterSupport1.length;
+            document.getElementById('numChampions').innerHTML = numSupport;
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        }  
+    })  
+
+
+let filterTank = document.getElementById('filter-cards-container');
+//FUNCIÓN PARA FILTRAR TANQUES
+document.querySelector('#tank').addEventListener('change', (event) =>{
+    if(event.target.checked){
+        document.getElementById('cards-container').style.display = "none";
+        document.getElementById('filter-cards-container').style.display = "flex";
+        let filterTank1 = filterData6(champions, 'Tank');
+        filterTank1.forEach(elemento => {
+            let resultTank = `
+                <div id="one-container" class="one-container">
+                    <img src="${elemento.splash}" class="img-container">
+                    <h4 id="description">${elemento.name}</h4>
+                </div>
+                `;
+            filterTank.insertAdjacentHTML("beforeend", resultTank);
+            });
+            let numTank = filterTank1.length;
+            document.getElementById('numChampions').innerHTML = numTank; 
+        }
+        else {
+            document.getElementById('cards-container').style.display = "flex";
+            document.getElementById('filter-cards-container').style.display = "none";
+            const championsNum = champions.length;
+            document.getElementById('numChampions').innerHTML = championsNum;
+        }  
+    })
+
+
+// function showFilterAZ(e) {
+//     document.querySelector("#az").textContent = e.detail;
+//     return sortData
+// }
+/////////////////////////////////////////////////////////////////////////////
+
+// let allAssassin = loll(champions, 'Assassin');{
+//     allAssassin = `
+//         <div id="one-container" class="one-container">
+//             <img src="${assassin.splash}" class="img-container">
+//             <h4 id="description">${assassin.name}</h4>
+//         </div>
+//         `;
+//     newCard.insertAdjacentHTML("beforeend", allAssassin);
+// };
+
+//Codigo Carlos
+// document.addEventListener('DOMContentLoaded',()=>{
+//     console.log("algo")
+//     assassin.addEventListener('change',(e)=>{
+//         // let filterAssassin = loll(champions, 'Assassin');
+//         // return filterAssassin;
+//         console.log("ya sirvo assassin", e.target.value)
+//     })
+//     //console.log(loll(champions, 'Assassin'));
+// })
+
+//const createCards = () => { 
+    champions.forEach(champion => {
     let card = `
-        <div id="one-container">
+        <div id="one-container" class="one-container">
             <img src="${champion.splash}" class="img-container">
             <h4 id="description">${champion.name}</h4>
         </div>
-    `;
+        `;
     newCard.insertAdjacentHTML("beforeend", card);
-});
-
-// champions.forEach(champion => {
-//     let card = `
-//         <div id="one-container" style="justify-content: space-evenly; float:left; margin: 0 1rem; width: 20%; height:100%; overflow: hidden;"><br>
-//             <img src="${champion.splash}" class="img-container" style="width: 100%; height: 100%; text-aling: center; margin-right: 0%;">
-//             <h4 id="description" style="background-color: #0F474A; font-family: 'Pirata One', cursive; font-size: 2em; text-aling: center; margin: 0 0; width: 100%;">${champion.name}</h4>
-//         </div>
-//     `;
-//     newCard.insertAdjacentHTML("beforeend", card);
-// });
-
-// document.querySelector('.goUp').addEventListener('click', () =>{
-//     window.scrollTo({
-//         top: 0,
-//         behavior:'smooth'
-//     });
-// });
-
-// document.getElementById('buttonStart').addEventListener('click', showButtonGoUp())
-// function showButtonGoUp(){
-//     document.querySelector('#goUp').style.display = 'block';
+    });
 // }
+
+//Barra buscadora de campeones
+//--------------------BUSCAR-------------------//
+const buscar = document.querySelector("#buscar");
+
+function filtro () {
+    newCard.innerHTML = "";
+    const texto = buscar.value.toLowerCase();
+    for(let champion of champions){
+        let nombre = champion.name.toLowerCase();
+        if(nombre.indexOf(texto) !== -1){
+            newCard.innerHTML += `
+                    <div id="one-container">
+                        <img src="${champion.splash}" class="img-container">
+                        <h4 id="description">${champion.name}</h4>
+                    </div>
+                `;
+        }
+    }
+        if(newCard.innerHTML === ""){
+            newCard.innerHTML = ` <p class="pNoFound">Campeón no encontrado</p> `;
+        }
+        
+}
+
+buscar.addEventListener("keyup", filtro);
+// //----------------------------------------//
 
 document.querySelector('#goUp').style.display = 'none';
 let showButtonGoUp = document.querySelector('#buttonStart');
@@ -43,4 +253,24 @@ showButtonGoUp.addEventListener('click', function(){
     document.querySelector('#goUp').style.display = 'block';
     document.querySelector('#homePage').style.display = 'none';
 })
-
+/////////////// FUNCIÓN QUE NOS AYUDÓ A CREAR CARLOS
+//let assassin = document.getElementById('assassin');
+//FUNCIÓN PARA FILTRAR ASESINOS
+// document.addEventListener('DOMContentLoaded',()=>{   
+//     assassin.addEventListener('change',(e)=>{
+//         document.getElementById('cards-container').style.display = "none";
+//         document.getElementById('filter-cards-container').style.display = "flex";
+//         let filterAssassin1 = loll(champions, 'Assassin');
+//         filterAssassin1.forEach(elemento => {
+//             let resultAssassin = `
+//                 <div id="one-container" class="one-container">
+//                     <img src="${elemento.splash}" class="img-container">
+//                     <h4 id="description">${elemento.name}</h4>
+//                 </div>
+//                 `;  
+//             filterAssassin.insertAdjacentHTML("beforeend", resultAssassin);
+//             });
+//             let numAssassin = filterAssassin1.length;
+//             document.getElementById('numChampions').innerHTML = numAssassin; 
+//     })
+// })
